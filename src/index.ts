0e9dc237ed80881f3334d2606151e745faecc53a
igor.efrem <igor.efrem@carasent.com>
2024-05-02 22:09:01 +0200

    chore: Updated package

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 export type * from './internal'
 declare global {
     interface Window {
-        __ASMA_CORE_UI__?: Awaited<typeof import('./internal')>
+        __ASMA_NOTISTACK__?: Awaited<typeof import('./internal')>
         rawWindow?: typeof window
     }
 }
 const realWindow = window.rawWindow || window
-let core_ui = realWindow.__ASMA_CORE_UI__
+let core_ui = realWindow.__ASMA_NOTISTACK__
 
 if (!core_ui) {
-    realWindow.__ASMA_CORE_UI__ = await import('./internal')
-    core_ui = realWindow.__ASMA_CORE_UI__
+    realWindow.__ASMA_NOTISTACK__ = await import('./internal')
+    core_ui = realWindow.__ASMA_NOTISTACK__
 }
 export const {
     SnackbarProvider,
