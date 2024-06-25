395abf28bb189686695032b2c68ec5c83d568350
Bularga Alexandru <bularga.alexandru@carasent.com>
2024-06-25 16:01:49 +0000

    Merged in feat/ASMA-4302 (pull request #1)
    Feat/ASMA-4302
    
    * v0.1.0
    
    * Removed unnecessary dependencies and exports

---
+++ src/snack-bar/story/StyledSnackbar.stories.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { StyledSnackbar } from '../StyledSnackbar'
+import { SnackbarProvider } from '../SnackbarProvider'
+import { Alert, Button, Stack } from '@mui/material'
+import { processInfoSnackbar } from '../processInfoSnackbar'
+
+const meta = {
+    title: 'Feedback/Styled Snackbar',
+    component: StyledSnackbar,
+    tags: [],
+    argTypes: {},
+    args: {},
+} satisfies Meta<typeof StyledSnackbar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SnackBar: Story = {
+    args: { ...meta.args },
+    render: () => <SnackbarExample />,
+}
+
+const SnackbarExample = () => {
+    const [openDefault, setOpenDefault] = useState(false)
+    const [openAlert, setOpenAlert] = useState(false)
+
+    const handleOpen = () => {
+        setOpenAlert(false)
+        setOpenDefault(true)
+    }
+
+    const handleClose = () => {
+        setOpenDefault(false)
+    }
+
+    const handleOpenAlert = () => {
+        setOpenDefault(false)
+        setOpenAlert(true)
+    }
+
+    const handleCloseAlert = () => {
+        setOpenAlert(false)
+    }
+
+    return (
+        <>
+            <SnackbarProvider autoHideDuration={3000} />
+            <Stack direction='column' spacing={2} sx={{ maxWidth: 400 }}>
+                <Button
+                    onClick={() => {
+                        processInfoSnackbar('Shared successfully!')
+                    }}
+                    variant='outlined'
+                >
+                    Show snackbar using notistack
+                </Button>
+
+                <Button variant='outlined' onClick={handleOpen}>
+                    Show default snackbar with action
+                </Button>
+                <StyledSnackbar
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={openDefault}
+                    onClose={handleClose}
+                    message='Nice default snack'
+                    action={
+                        <Button variant='text' onClick={handleClose} color='inherit'>
+                            Close
+                        </Button>
+                    }
+                />
+
+                <Button variant='outlined' onClick={handleOpenAlert}>
+                    Show snackbar with alert
+                </Button>
+                <StyledSnackbar
+                    open={openAlert}
+                    onClose={handleCloseAlert}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                >
+                    <Alert onClose={handleCloseAlert} severity='success' variant='filled' sx={{ width: '100%' }}>
+                        Nice alert snack
+                    </Alert>
+                </StyledSnackbar>
+            </Stack>
+        </>
+    )
+}
--- src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.json' {
-    const value: { [key: string]: any }
-    export default value
-}
